6ef2c6041545b24612100bcd68ee35cc10729eab
chocolado4kaa <chocolado4kaa12@gmail.com>
2025-08-19 13:42:29 +0300

    init porject; add header; add dark theme

---
+++ Weather/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import Header from './components/Header/Header.jsx';
+import Div from './components/Div/Div.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <main className="main-content mt-10">
+        <Div className="w-full main-div h-50">
+
+        </Div>
+     </main>
+    </div>
+  )
+}
+
+export default App
